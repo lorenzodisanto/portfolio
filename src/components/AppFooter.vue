2d9be3b8449b8222e3d23395d5ec8de292c948b4
lorenzodisanto <lore.disanto95@gmail.com>
2024-06-18 12:20:17 +0200

    feat: responsive footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,28 +8,34 @@ export default {
 <template>
   <footer>
     <nav class="navbar">
-      <div class="container d-flex justify-content-between">
-        <div>
+      <div class="container">
+        <div class="name">
           <span class="navbar-brand text-white" href="" target="_blank">
             &copy; {{ author }} 2024
           </span>
         </div>
         <div class="social">
-          <a
-            href="https://www.linkedin.com/in/lorenzo-di-santo-454282266/"
-            target="_blank"
-          >
-            <font-awesome-icon icon="fa-brands fa-linkedin" />
-            LinkedIn
-          </a>
-          <a href="https://github.com/lorenzodisanto" target="_blank">
-            <font-awesome-icon icon="fa-brands fa-github" />
-            GitHub
-          </a>
-          <a href="https://www.instagram.com/llor.ds/" target="_blank">
-            <font-awesome-icon icon="fa-brands fa-instagram" />
-            Instagram
-          </a>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/lorenzo-di-santo-454282266/"
+              target="_blank"
+            >
+              <font-awesome-icon icon="fa-brands fa-linkedin" />
+              LinkedIn
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/lorenzodisanto" target="_blank">
+              <font-awesome-icon icon="fa-brands fa-github" />
+              GitHub
+            </a>
+          </div>
+          <div>
+            <a href="https://www.instagram.com/llor.ds/" target="_blank">
+              <font-awesome-icon icon="fa-brands fa-instagram" />
+              Instagram
+            </a>
+          </div>
         </div>
       </div>
     </nav>
@@ -38,20 +44,53 @@ export default {
 
 <style lang="scss" scoped>
 footer {
-  height: 56px;
+  height: 200px;
   background-color: #171b20;
 
+  .name {
+    width: 100%;
+    text-align: center;
+    margin: 8px 0px;
+  }
   .social {
-    a {
-      text-decoration: none;
-      margin-left: 60px;
-      font-size: 1.2rem;
-      color: #eeeeee;
+    width: 100%;
+    text-align: center;
+    div {
+      margin: 10px 0px;
+      a {
+        text-decoration: none;
+        font-size: 1.2rem;
+        color: #eeeeee;
+      }
     }
   }
 
   .social a:hover {
     color: #ff5722;
+  }
+}
+
+@media screen and (min-width: 992px) {
+  footer {
+    height: 56px;
+
+    .container {
+      display: flex;
+      justify-content: space-between;
+
+      .name {
+        text-align: start;
+        width: 50%;
+      }
+      .social {
+        width: 50%;
+        display: flex;
+        justify-content: flex-end;
+        div {
+          margin-left: 35px;
+        }
+      }
+    }
   }
 }
 </style>
