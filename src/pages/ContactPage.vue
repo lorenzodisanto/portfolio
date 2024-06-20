b3d8b8c441e91f5d5e5ff83f6258b3c9cbb50dfc
lorenzodisanto <lore.disanto95@gmail.com>
2024-06-21 00:12:19 +0200

    feat: contact page responsive

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -9,26 +9,34 @@ export default {
 </script>
 
 <template>
-  <div class="wrapper container py-5">
+  <div class="wrapper container">
+    <h1 class="py-5">{{ title }}</h1>
     <div class="contacts">
-      <!-- <h1>{{ title }}</h1> -->
-      <p>If you want to talk to me,</p>
-      <p>you can send an email to me:</p>
+      <p class="mb-3">If you want to talk to me,</p>
+      <p class="mb-3">you can send an email to me:</p>
       <p>
         <a href="mailto:lore.disanto95@gmail.com" style="color: #ff5722"
           >lore.disanto95@gmail.com</a
         >
       </p>
-      <p>Visit my profiles v</p>
+      <!-- <p>Visit my profiles v</p> -->
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.wrapper {
+  display: block;
+}
 h1 {
   font-weight: 600;
 }
 .contacts {
-  font-size: 2rem;
+  font-size: 1.3rem;
+}
+@media screen and (min-width: 576px) {
+  .contacts {
+    font-size: 2rem;
+  }
 }
 </style>
